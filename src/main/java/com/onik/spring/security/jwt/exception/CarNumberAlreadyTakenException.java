78de3a9f7fcbd8405922fabfd5601cc2537d15e1
onikm <onik_mkrtchyan@mail.ru>
2022-02-26 20:02:33 +0000

    Improved functionality

--- src/main/java/com/onik/spring/security/jwt/exception/CarNumberAlreadyTakenException.java
+++ src/main/java/com/onik/spring/security/jwt/exception/CarNumberAlreadyTakenException.java
@@ -1,11 +1,7 @@
 package com.onik.spring.security.jwt.exception;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 public class CarNumberAlreadyTakenException extends RuntimeException {
     public CarNumberAlreadyTakenException(String carNumber) {
+        super("Car number already exists: " + carNumber);
     }
 }
